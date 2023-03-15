db80f3d8300c3c335c97a52fc83b0303fb582a9f
Георгий Петренко <58736243+Georgy87@users.noreply.github.com>
2023-03-15 15:18:29 +0300

    update type for hooks

--- src/hooks/useContinentLoader.tsx
+++ src/hooks/useContinentLoader.tsx
@@ -14,16 +14,18 @@ export const useContinentLoader = ({ continent }: Props) => {
 
   const delays = useSelector(selectLocalsLatensy);
   const rerenderedGlobalDistribution = useSelector(selectRerenderedGlobalDistribution);
-  
+
   const [loading, setLoading] = useState<boolean>(true);
   const [time, setTime] = useState<number>(0);
-  
+
   const delay = useMemo(() => {
     if (rerenderedGlobalDistribution.length) {
+      //@ts-ignore
       return rerenderedGlobalDistribution.find((latency: { [key: string]: number }) => continent in latency)?.[
         continent
       ];
     }
+    //@ts-ignore
     return delays.find((latency: { [key: string]: number }) => continent in latency)?.[continent];
   }, [continent, delays, rerenderedGlobalDistribution]);
 
@@ -31,13 +33,13 @@ export const useContinentLoader = ({ continent }: Props) => {
     if (!delay) return;
 
     const d: number = delay * 50;
-    
+
     const timer = setTimeout(() => {
       dispatch(setTimeoutsCounter({ counter: 1, delay: delay }));
       setTime(delay * 1000);
       setLoading(false);
     }, d);
-   
+
     if (loading === false) {
       clearTimeout(timer);
     }
@@ -46,7 +48,7 @@ export const useContinentLoader = ({ continent }: Props) => {
       clearTimeout(timer);
     };
   }, [delay, loading, dispatch]);
-  
+
   return {
     loading,
     delay,
